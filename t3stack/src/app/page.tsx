12c8e80a60d00e4fa5e116591ff8d6e1662e255e
fidaatag <fidaa2fidaa@gmail.com>
2025-04-19 23:28:46 +0700

    feat: layout landing-page

--- t3stack/src/app/page.tsx
+++ t3stack/src/app/page.tsx
@@ -1,14 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { HeroCanvas } from "@/components/hero/hero-canvas";
+import { NavbarMain } from "@/components/navbar-main";
 import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
 
   return (
     <HydrateClient>
-      <div>
-        "Hello world!"
-      </div>
-      <Button>Ok button</Button>
+      <NavbarMain />
+      <HeroCanvas />
     </HydrateClient>
   );
 }
