8936d809029c4de1a5ecf6e8e1a7aa77f1745f55
fidaatag <fidaa2fidaa@gmail.com>
2025-04-19 22:17:51 +0700

    chore: delete default t3-stack

--- t3stack/src/server/api/root.ts
+++ t3stack/src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { cainRouter } from "./routers/cain";
 
@@ -8,7 +7,6 @@ import { cainRouter } from "./routers/cain";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   cain: cainRouter
 });
 
