2c47993ffe21dd6dfda384794cf88d61c17afd54
fidaatag <fidaa2fidaa@gmail.com>
2025-04-21 10:17:00 +0700

    feat: updare server api trpc after downgrade v to 4, change auth to getServerAuthSesion

--- t3stack/src/server/api/trpc.ts
+++ t3stack/src/server/api/trpc.ts
@@ -11,8 +11,8 @@ import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
-import { auth } from "@/server/auth";
 import { db } from "@/server/db";
+import { getServerAuthSession } from "@/server/auth/config";
 
 /**
  * 1. CONTEXT
@@ -27,7 +27,7 @@ import { db } from "@/server/db";
  * @see https://trpc.io/docs/server/context
  */
 export const createTRPCContext = async (opts: { headers: Headers }) => {
-  const session = await auth();
+  const session = await getServerAuthSession();
 
   return {
     db,
